c89ef40f50ccb4684072907957fa8b5a715651f4
Jason Rhinelander <jason@imaginary.ca>
2021-03-03 16:18:06 -0400

    Revert "Debug: increase router tick interval to 20s"
    This reverts commit 15760b2bc7b75ddc7ea2685a20fc17ab5e4bcb32.

--- llarp/router/router.cpp
+++ llarp/router/router.cpp
@@ -42,7 +42,7 @@
 
 #include <oxenmq/oxenmq.h>
 
-static constexpr std::chrono::milliseconds ROUTER_TICK_INTERVAL = 20s;
+static constexpr std::chrono::milliseconds ROUTER_TICK_INTERVAL = 1s;
 
 namespace llarp
 {
