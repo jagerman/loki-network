4cefe29c5223d58190cc8ef88eef2a94dcd5b8ed
Jason Rhinelander <jason@imaginary.ca>
2021-03-03 12:56:10 -0400

    Revert me: flush harder

--- llarp/handlers/tun.cpp
+++ llarp/handlers/tun.cpp
@@ -829,7 +829,8 @@ namespace llarp
         LogInfo(Name(), " has ipv6 address ", m_OurIPv6);
       }
 
-      Router()->loop()->add_ticker([this] { Flush(); });
+      //Router()->loop()->add_ticker([this] { Flush(); });
+      Router()->loop()->call_every(5ms, weak_from_this(), [this] { Flush(); });
 
       if (m_OnUp)
       {
